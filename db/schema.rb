a5299e3d27687da8240c084079f4bf7c2fd124a0
Brad Watson <brad.watson.orlando@gmail.com>
2018-10-03 08:52:34 -0400

    after updating

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170603151548) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.string "name"
